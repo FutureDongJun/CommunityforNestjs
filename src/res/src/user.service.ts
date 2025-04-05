73e3f1a5d1b5cfe8fb3543acef19688d1b71f00f
FutureDongJun <kdjun1234@gmail.com>
2025-04-05 16:42:31 +0900

    처음으로 오류가 떴었는데, 상대경로의 설정을 잘못했음. 다시 고침

--- src/res/src/user.service.ts
+++ src/res/src/user.service.ts
@@ -1,8 +1,39 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserEntity } from "src/entities/user.entity";
+import { Repository } from "typeorm";
+import { hash } from 'bcrypt'
 
 @Injectable()
 export class UserService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>,
+    ) { }
+
     async getMainPage() {
         return 'User Main Page';
     }
+
+    async register(email: string, password: string) {
+        const existedUser = await this.userRepository.findOne({
+            where: {
+                email: email,
+            },
+        }); // userRepository의 fineOne 함수를 이용해서 email 칼럼의 값이 입력된 변수에 담긴 이메일과 같은 user를 select 함.
+
+        if (existedUser) {
+            throw new BadRequestException('이미 해당 이메일이 존재합니다.');
+        }
+
+        const hashedPassword = await hash(password, 10);
+
+        const user = await this.userRepository.save({
+            email: email,
+            password: hashedPassword,
+        });
+
+        return user;
+    }
+
 }
