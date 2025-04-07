790290f78e9f0580160d0e361b7f528a93675ca0
FutureDongJun <kdjun1234@gmail.com>
2025-04-07 23:13:23 +0900

    더 열심히 하자

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -6,7 +6,7 @@ import { CommonBigPKEntity } from './common.entity';
 
 @Entity('User') // 테이블 이름을 User로 설정해주는 데코레이터
 export class UserEntity extends CommonBigPKEntity {
-    @Column('varchar', { unique: true, nullable: false}) // 일반적인 칼럼
+    @Column('varchar', { unique: true, nullable: false}) // 일반적인 칼럼.
     email: string;
 
     @Column('varchar', { unique: false, nullable: false})
