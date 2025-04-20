899344e50fd658fc20f86febdb3c9ebc5da6bf21
FutureDongJun <kdjun1234@gmail.com>
2025-04-20 20:47:15 +0900

    Swagger 도구

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+  .setTitle('API 문서 제목')
+  .setDescription('API 문서 설명')
+  .setVersion('0.0.1')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document); // 'api-docs'는 swagger 문서로 접속할 url임.
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
