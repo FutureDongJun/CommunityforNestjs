94809c8edaaf176e95e75f1670197273262ad6f7
FutureDongJun <kdjun1234@gmail.com>
2025-04-25 14:33:48 +0900

    feat: HttpException Filter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './filter/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter);
 
   const options = new DocumentBuilder()
   .setTitle('API 문서 제목')
@@ -14,7 +16,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api-docs', app, document); // 'api-docs'는 swagger 문서로 접속할 url임.
-
+  
 
   await app.listen(process.env.PORT ?? 3000);
 }
